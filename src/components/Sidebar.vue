b1450fd85d2ec28af637a2db4ee3462d6d4af569
Samuel Henryk De Souza Ziger <srziger3@gmail.com>
2025-03-28 17:05:10 -0300

    Alteração na página de perfil, foram feitas alterações nos dados do perfil

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,67 +5,77 @@
         <button title="Fermer Profil">X</button>
       </div>
       <div class="photo">
-        <img :src="getPhoto()" alt="Développeur web sur Pau, Orthez et Tarbes" width="300" height="356">
+        <img
+          :src="getPhoto()"
+          alt="Développeur web sur Pau, Orthez et Tarbes"
+          width="300"
+          height="356" />
       </div>
     </div>
     <div class="sidebar-content">
-      <h2>Informations</h2>
+      <br />
+      <h2>Informações</h2>
       <p class="border-blue">
-        <img src="~@/assets/img/photo-title.png" width="115" height="70" class="img-title"> AGULLO<br>
-        07 52 63 77 22<br>
-        contact@jagullo.fr
+        Samuel Henryk <br />
+        (61)9-86536975<br />
+        samuelziger01@gmail.com
       </p>
-      <h2>Compétences</h2>
+      <h2>HABILIDADES</h2>
       <p class="border-orange">
-        Gestion projet <span class="attr">[5/5]</span><br>
-        Développement <span class="attr">[4/5]</span><br>
-        Optimisation SEO <span class="attr">[4/5]</span><br>
-        Design <span class="attr">[4/5]</span><br>
-        Rédaction <span class="attr">[3,5/5]</span><br>
-        Veille stratégique <span class="attr">[3/5]</span>
+        Desenvolvimento Web <span class="attr">[4,5/5]</span><br />
+        Backend & Banco de Dados <span class="attr">[4/5]</span><br />
+        Gestão de Projetos <span class="attr">[3/5]</span><br />
+        Design <span class="attr">[4/5]</span><br />
+        Otimização de SEO <span class="attr">[3,5/5]</span><br />
+        Cibersegurança & Redes <span class="attr">[3/5]</span>
       </p>
-      <h2>Connaissances</h2>
+      <h2>Conhecimento</h2>
       <p class="border-red">
-        <span class="title">Outils</span><br>
-        PhpStorm, VS Code, Postman, phpMyAdmin<br>
-        Apache, IIS, Node.js, Composer, Npm<br>
-        Git, GitKraken, GitHub, Subversion, SSH<br>
-        WordPress, WooCommerce, PrestaShop<br>
-        Screaming Frog, Ubersuggest, Yourtext.guru<br>
-        Adobe Photoshop, Illustrator, InDesign<br>
-        Affinity Photo, Designer, Publisher<br>
-        Google Analytics, Search Console, Lighthouse<br>
-        Microsoft Word, Excel, Graph API
-      </p>
-      <p class="border-red">
-        <span class="title">Langages</span><br>
-        PHP, JavaScript, MySQL, MariaDB<br>
-        CSS, LESS/SCSS, HTML, Schema.org<br>
-        Framework : Symfony, Bootstrap, Vue.js, jQuery
+        <span class="title"></span><br />
+        Desenvolvimento Web (Front-end & Back-end)<br />
+        Arquitetura de Software, <br />
+        Git, GitKraken, GitHub, SEO e Otimização de Sites<br />
+        WordPress, WooCommerce, PrestaShop<br />
+        Cibersegurança e Redes, Yourtext.guru<br />
+        Adobe Photoshop, Illustrator, InDesign<br />
+        Gestão de Projetos e Versionamento<br />
+        <strong>Ferramentas</strong><br />
+        Edição & Desenvolvimento: PhpStorm, VS Code, Postman, phpMyAdmin<br />
+        Servidores & Gerenciamento: Apache, IIS, Node.js, Composer, Npm<br />
       </p>
       <p class="border-red">
-        <span class="title">Langue vivante</span><br>
-        Anglais élémentaire et technique
+        <span class="title">Linguagens</span><br />
+        PHP, JavaScript, MySQL,<br />
+        CSS, LESS/SCSS, HTML,<br />
+        Framework :Bootstrap, Vue.js, jQuery<br />
+        Java
       </p>
-      <h2>Centres d'intérêt</h2>
+      <p class="border-red">
+        <span class="title">Línguas Faladas</span><br />
+        Português(Nativo)<br />Inglês (Intermédiario)
+      </p>
+      <h2>Interesses</h2>
       <p class="border-blue">
-        <span class="title">Général</span><br>
-        Littérature, Cinéma, Science, Écologie
+        <span class="title">Geral</span><br />
+        Xadrez,Religião,Militarismo, Tecnologia
       </p>
       <p class="border-blue">
-        <span class="title">Technologie</span><br>
-        Informatique, Information & Communication
+        <span class="title">Tecnologia</span><br />
+        da Computação , Informação e Comunicação
       </p>
       <p class="border-blue">
-        <span class="title">Sport</span><br>
-        Running, Fitness, Basket, Tennis
+        <span class="title">Esporte</span><br />
+        Automobilismo, Corrida
       </p>
-      <h2>Téléchargement</h2>
-      <a href="/static/cv-jagullo-2024.pdf"
-         rel="nofollow noreferrer"
-         target="_blank"
-         class="btn btn-primary btn-cv"
-         title="Télécharger le CV en PDF">CV PDF</a>
+      <h2>Download</h2>
+      <a
+        href="/static/cv-jagullo-2024.pdf"
+        rel="nofollow noreferrer"
+        target="_blank"
+        class="btn btn-primary btn-cv"
+        title="Télécharger le CV en PDF"
+        >CV PDF</a
+      >
     </div>
   </div>
 </template>
@@ -83,12 +93,15 @@ export default {
     }
   },
   mounted() {
-    this.scrollBar = Scrollbar.init(document.querySelector('#sidebar'), {damping: 0.5})
+    this.scrollBar = Scrollbar.init(document.querySelector('#sidebar'), {
+      damping: 0.5,
+    })
     this.init()
   },
   methods: {
     init() {
-      document.getElementById('sidebar').style.height = window.innerHeight + 'px'
+      document.getElementById('sidebar').style.height =
+        window.innerHeight + 'px'
     },
     close() {
       this.open = false
@@ -111,12 +124,16 @@ export default {
       this.$emit('open-sidebar', this.open)
     },
     showScroll() {
-      const scrollbar = document.getElementById('sidebar').getElementsByClassName('scrollbar-track-y')[0]
+      const scrollbar = document
+        .getElementById('sidebar')
+        .getElementsByClassName('scrollbar-track-y')[0]
       scrollbar.classList.add('show')
       this.scrollBar.update()
     },
     hideScroll() {
-      const scrollbar = document.getElementById('sidebar').getElementsByClassName('scrollbar-track-y')[0]
+      const scrollbar = document
+        .getElementById('sidebar')
+        .getElementsByClassName('scrollbar-track-y')[0]
       scrollbar.classList.remove('show')
       this.scrollBar.update()
     },
