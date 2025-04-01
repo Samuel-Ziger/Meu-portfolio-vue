dd719ac17a8e399e1db5c1af9b43697c3c21f6e0
Samuel Henryk De Souza Ziger <srziger3@gmail.com>
2025-04-01 00:15:01 -0300

    Alteração e adcição na página de fontes

--- src/views/Source.vue
+++ src/views/Source.vue
@@ -39,8 +39,6 @@
     <section class="project cookie-wall">
       <h2>Meu Portfolio Dev</h2>
       <span class="archived">Arquivo</span>
-      <img src="*" class="logo" width="102" height="25" alt="" />
-      <img src="*" class="logo dark" width="102" height="25" alt="" />
       <p>
         Um site de portfólio pessoal para mostrar meus projetos e habilidades
         como desenvolvedor web. O site é responsivo e otimizado para
