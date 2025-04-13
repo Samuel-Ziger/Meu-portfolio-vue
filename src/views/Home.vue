86479d04f8d48fd0f109141f78a4aa4987d7e3f5
Samuel Henryk De Souza Ziger <srziger3@gmail.com>
2025-04-13 16:45:48 -0300

    Teste sem foto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -449,7 +449,7 @@
                 <div class="carousel-item active" data-bs-interval="10000">
                   <picture>
                     <img
-                      :src="require('')"
+                      :src="projeto1"
                       class="d-block w-100"
                       alt="Projeto 1"
                     />
@@ -466,7 +466,7 @@
                 <div class="carousel-item" data-bs-interval="10000">
                   <picture>
                     <img
-                      :src="require('')"
+                      :src="projeto2"
                       class="d-block w-100"
                       alt="Projeto 2"
                     />
@@ -628,6 +628,8 @@ import Scrollbar from 'smooth-scrollbar'
 import anime from 'animejs/lib/anime.es.js'
 import Techno from '@/components/Techno'
 import 'bootstrap/js/dist/carousel'
+import projeto1 from '../assets/img/projeto1.png'
+import projeto2 from '../assets/img/projeto2.png'
 
 export default {
   name: 'Home',
@@ -636,7 +638,9 @@ export default {
   },
   data() {
     return {
-      scrollBar: Scrollbar
+      scrollBar: Scrollbar,
+      projeto1,
+      projeto2
     }
   },
   mounted() {
