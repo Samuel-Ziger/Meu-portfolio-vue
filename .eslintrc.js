da9b01e16670fc18d353d3f5a88d2bd9dd15950b
Samuel Henryk De Souza Ziger <srziger3@gmail.com>
2025-04-13 15:52:18 -0300

    ajuste proxy

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-env"]
+    }
   },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+module.exports = { 
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '',
+  devServer: {
+    proxy: 'http://localhost:3000'
+  },
   chainWebpack: config => {
     config.plugin('html')
       .tap(args => {
